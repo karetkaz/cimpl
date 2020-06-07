8d8331880d1c2e23b70f025a6bb64a5d86ac20f1
Karetka Mezei Zoltan <karetkaz@gmail.com>
2020-06-07 14:23:41 +0300

    added jpeg and png to win32 canAssign fixes windowing fixes

---
+++ cmplGfx/libs/libjpeg/jpeg.h
@@ -0,0 +1,3 @@
+#include "jconfig.h"
+#define JCONFIG_INCLUDED
+#include "src/jpeg-8d/jpeglib.h"
---
+++ cmplGfx/libs/libpng/png.h
@@ -0,0 +1,4 @@
+//~ #define PNGLCONF_H
+#include "pnglibconf.h"
+#include "src/zlib-1.2.5/zlib.h"
+#include "src/libpng-1.5.7/png.h"
